b659fb02056136519fbbc256f3236f809ec959d8
Stones <1181322273@qq.com>
2024-01-17 17:34:27 +0800

    command sleep

---
+++ user/sleep.c
@@ -0,0 +1,16 @@
+// 从shell中获取命令和参数，且获取的内容均为string类型
+// 最终使用系统调用sleep来实现
+// 如果有错误，则输出。
+#include <kernel/types.h> //  定义了常用的类型
+#include <user/user.h> // 定义了一些用户函数，例如read
+
+int main(int argc, char *argv[]){
+	if(argc<=1){
+		fprintf(2, "missing parameters!");
+		exit(1);
+	}
+	char *misc = argv[1];
+	int sec = atoi(misc);
+	exit(sleep(sec));
+}
+
