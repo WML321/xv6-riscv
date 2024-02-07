bce13d38b9922a4919a1d8afe721027cb877de31
WML321 <1181322273@qq.com>
2024-02-07 16:10:11 +0800

    print a page

--- kernel/exec.c
+++ kernel/exec.c
@@ -19,6 +19,41 @@ int flags2perm(int flags)
     return perm;
 }
 
+void vmprint(pagetable_t pagetable, int depth){
+  char p[9];// 这是前缀
+  p[0]='.';
+  p[1]='.';
+  p[2]='\0';
+
+  if(depth>=2){
+    p[2]=' ';
+    p[3]='.';
+    p[4]='.';
+    if(depth==3){
+      p[5]=' ';
+      p[6]='.';
+      p[7]='.';
+      p[8]='\0';
+    }else{
+      p[5]='\0';
+    }
+  }
+  if(depth==1){
+    printf("page table %p\n", pagetable);
+  }
+  for(int i=0;i<512;i++){
+    pte_t pte = pagetable[i];
+    if(pte & PTE_V){
+      uint64 child = PTE2PA(pte);
+
+      printf("%s%d: pte %p pa %p\n", p, i, pte, child);
+      if(depth<3) vmprint((pagetable_t)child, depth+1);
+    }
+  }
+  return;
+}
+
+
 int
 exec(char *path, char **argv)
 {
@@ -127,7 +162,9 @@ exec(char *path, char **argv)
   p->trapframe->epc = elf.entry;  // initial program counter = main
   p->trapframe->sp = sp; // initial stack pointer
   proc_freepagetable(oldpagetable, oldsz);
+  
 
+  if(p->pid==1) vmprint(p->pagetable, 1);
   return argc; // this ends up in a0, the first argument to main(argc, argv)
 
  bad:
